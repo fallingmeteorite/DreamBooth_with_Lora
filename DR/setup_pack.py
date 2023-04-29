37930ab660ea639d2b2fa855dac64bdbb505bf39
fallingmeteorite <2327667836@qq.com>
2023-04-29 13:52:15 +0800

    update

--- DR/setup_pack.py
+++ DR/setup_pack.py
@@ -1,10 +1,8 @@
 import os
+import sys
 import time
+import sys
 
-net_one=(r'https://pypi.tuna.tsinghua.edu.cn/simple')
-net_two=(r'http://mirrors.aliyun.com/pypi/simple/')
-net_three=(r'https://pypi.mirrors.ustc.edu.cn/simple/')
-net_four=(r'http://pypi.douban.com/simple/')
 while True:
     print(r' 1 清华镜像源')
     print(r' 2 阿里云镜像源')
@@ -36,13 +34,25 @@ while True:
         print(r'输入错误，或未输入，请重新选择')
     time.sleep(4)
 
+while True:
+    buer_three = input(r'请问你是否想要更新(第一次安装不选)yes/no:')
+    if buer_two == 'yes':
+        print(r'开始更新')
+        os.system(r'pip install --use-pep517 --upgrade -r DE\requirements.txt --no-warn-script-location -i ' + net)
+        sys.exit(0)
+        break
+    elif buer_two == 'no':
+        print(r'跳过')
+        break
+    else:
+        print(r'输入错误，或未输入，请重新选择')
+
 print(r'开始安装依赖包')
 os.system(r'python -V')
 try:
     os.system(r'pip install torch-2.0.0.dev20230202+cu116-cp310-cp310-win_amd64.whl')
     os.system(r'pip install torchvision-0.15.0.dev20230206+cu116-cp310-cp310-win_amd64.whl')
     os.system(r'pip install torchaudio-2.0.0.dev20230206+cu116-cp310-cp310-win_amd64.whl')
-    os.system(r'pip install dynamo==0.1.1')
     print(r"==========================================================================================================")
     print(r"torch安装成功")
 except:
@@ -51,7 +61,7 @@ except:
 try:
     os.system(r'pip install --use-pep517 --upgrade -r DE\requirements.txt --no-warn-script-location -i '+net)
     os.system(r'pip install xformers-0.0.17+b89a493.d20230304-cp310-cp310-win_amd64.whl')
-    os.system(r'pip install triton-2.0.0-cp310-cp310-win_amd64.whl')
+    os.system(r'pip install deepspeed-0.8.1+unknown-py3-none-any.whl')
     print(r"==========================================================================================================")
     print(r"依赖安装成功")
 except:
